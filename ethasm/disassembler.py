70ed6d32c8736d23bc727da108c14f8a66048d80
Nathan Wilcox <nejucomo@gmail.com>
2014-08-25 15:07:06 -0700

    Tweak the assembler output, adding byte offsets and push argument decimal / string representations.

--- ethasm/disassembler.py
+++ ethasm/disassembler.py
@@ -1,12 +1,14 @@
-from pyethereuem.opcodes import opcodes, reverse_opcodes
+#from pprint import pprint
+
+from pyethereum.opcodes import opcodes, reverse_opcodes
 
 PUSH_BASE = reverse_opcodes['PUSH']
 PUSH_CEIL = PUSH_BASE + 32
 
 
 def disassemble(inf, outf):
-    for instruction in iter_dis(inf):
-        outf.write('{0}\n'.format(instruction))
+    for ix, instruction in iter_dis(inf):
+        outf.write('{0: 3d}: {1}\n'.format(ix, instruction))
 
 
 def iter_dis(inf):
@@ -15,11 +17,30 @@ def iter_dis(inf):
         opcode = ord(c)
 
         if PUSH_BASE <= opcode < PUSH_CEIL:
-            arg = read_push_arg(it, opcode - PUSH_BASE)
-            yield 'PUSH 0x{0:s}'.format(arg)
+            arglen = 1 + opcode - PUSH_BASE
+            arglelist = read_push_arg(it, arglen)
+            argbehex = ''.join(reversed(arglelist)).encode('hex')
+            argchars = []
+            for c in arglelist:
+                b = ord(c)
+                if 0x20 <= b < 0x80:
+                    argchars.append(c)
+                else:
+                    argchars = None
+                    break
+
+            if argchars is None:
+                argstr = ''
+            else:
+                argstr = repr(''.join(argchars))
+
+            yield i, 'push 0x{0:s} = {1} {2}'.format(
+                        argbehex,
+                        long(argbehex, 16),
+                        argstr)
         else:
             try:
-                yield opcodes[c][0]
+                yield i, opcodes[opcode][0].lower()
             except KeyError:
                 raise ValueError('Unknown opcode at byte index {0}: 0x{1:x}'.format(i, opcode))
 
@@ -37,4 +58,4 @@ def read_push_arg(it, count):
     while len(bytes) < count:
         (_, byte) = it.next()
         bytes.append(byte)
-    return ''.join(bytes)
+    return bytes
