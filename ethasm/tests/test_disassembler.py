99117d8b2b10210e1378171a1a3ae0b66609a354
Nathan Wilcox <nejucomo@gmail.com>
2014-08-28 08:16:21 -0700

    Start testing `PUSH1` disassembly.  This exposes an asymmetry in string representation.

--- ethasm/tests/test_disassembler.py
+++ ethasm/tests/test_disassembler.py
@@ -2,7 +2,26 @@ import unittest
 from cStringIO import StringIO
 import string
 
+from pyethereum import opcodes
 from ethasm import disassembler
+
+
+class dis_push_Tests (unittest.TestCase):
+
+    def test_push1_nonprintable(self):
+        self._test_push1('\x17', 'push 0x17 ; 23 ')
+
+    def test_push1_printable(self):
+        self._test_push1('*', 'push 0x2a ; 42 "*"')
+
+    def test_push1_printable_dquote(self):
+        self._test_push1('*', r'push 0x2a ; 42 "\""')
+
+    def _test_push1(self, char, expectedinstruction):
+        opcode = opcodes.reverse_opcodes['PUSH1']
+        it = iter([(43, char)])
+        inst = disassembler.dis_push(it, 42, chr(opcode), opcode)
+        self.assertEqual(inst, expectedinstruction)
 
 
 class dis_other_Tests (unittest.TestCase):
